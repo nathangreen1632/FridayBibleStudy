a526502229ca1660f7214f2c6e3126c42d13bb1e
Nathan Green <ngreen1632@gmail.com>
2025-08-29 06:46:08 -0500

    feat(client): add Footer component to App layout
    - Integrate `Footer` component into the main `App` layout for consistent footer visibility across the app.
    - Adjust `main` element padding to ensure proper spacing and layout alignment.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import AppRoutes from './AppRoutes';
 import { Toaster } from 'react-hot-toast';
 import Navbar from "./components/nav/Navbar.tsx";
+import Footer from "./common/Footer.tsx";
 
 export default function App(): React.ReactElement {
   return (
     <>
       <Navbar />
-      <main className="max-w-5xl mx-auto p-6">
+      <main className="max-w-5xl mx-auto p-3">
         <AppRoutes />
       </main>
+      <Footer />
       <Toaster position="top-center" />
     </>
   );
