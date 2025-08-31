d3fa358b308aac0d42345a48fded5803585945ca
Nathan Green <ngreen1632@gmail.com>
2025-08-31 12:20:31 -0500

    refactor(client): update `HomePage` spacing for consistency
    - Adjust spacing between components for improved alignment and layout consistency.
    - Simplify class definitions by reducing excess whitespace.

--- Client/src/pages/HomePage.tsx
+++ Client/src/pages/HomePage.tsx
@@ -6,9 +6,9 @@ import PhilosophyView from '../jsx/philosophyView';
 export default function HomePage(): React.ReactElement {
   return (
     <main className="bg-[var(--theme-bg)] text-[var(--theme-text)] min-h-[84vh] flex flex-col pt-4 sm:pt-6">
-      <div className="flex-grow space-y-6 sm:space-y-10">
-        <HeroSectionView />
-        <PhilosophyView />
+      <div className="flex-grow space-y-3 sm:space-y-6">
+        <HeroSectionView/>
+        <PhilosophyView/>
       </div>
     </main>
   );
